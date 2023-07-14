ae6345735e41e2d27a643cf72b94e5ac47282466
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-14 15:59:15 -0500

    solve linter issues

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ if (!localStorage.getItem('appId')) await appId();
 const pokemonData = [];
 
 const storage = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 for (let i = 1; i < storage.length; i += 1) {
   const pokem = new Pokemon(i);
   const pokemon = await pokem.data();
