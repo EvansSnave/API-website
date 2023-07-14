3823e628bafb1f29e6b1e1d370a7cff0697c844f
akbrsazish <sazishaliakbar@gmail.com>
2023-07-14 18:50:53 +0330

    Add commentButton.js file

---
+++ src/modules/commentButton.js
@@ -0,0 +1,31 @@
+import closeWindow from './closeButton';
+import createComment from './createCommentDOM';
+import Pokemon from './createPokemon';
+import submit from './submitComment';
+import loading from './loadComments'
+
+const commentButton = () => {
+  const buttons = document.querySelectorAll('.comments');
+  buttons.forEach(async (button, index) => {
+    const pokemon = new Pokemon(index + 1);
+    const newPokemon = await pokemon.data();
+    button.addEventListener('click', () => {
+      const card = {
+        image: newPokemon.sprites.front_default,
+        name: newPokemon.name,
+        weight: newPokemon.weight,
+        ability: newPokemon.abilities[0].ability.name,
+        type: newPokemon.types[0].type.name,
+        move: newPokemon.moves[0].move.name,
+        id: newPokemon.id,
+      };
+      createComment(card);
+      closeWindow();
+      submit(card.id);
+      loading(card.id);
+      document.body.style.overflow = 'hidden';
+    })
+  })
+}
+
+export default commentButton;
